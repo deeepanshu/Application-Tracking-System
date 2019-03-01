c6d16409471c2aaa68568078525d90026d7577a3
Deepanshu Jain <deepanshujain1234@live.com>
2019-03-01 15:38:33 +0530

    Fixed Dept. Profile

--- controller/auth/auth.controller.js
+++ controller/auth/auth.controller.js
@@ -8,44 +8,73 @@ module.exports = {
         res.json(req.user);
     },
     register: (req, res) => {
-        if(!req.body.email || !req.body.password) {
-            res.json({success: false, message: 'Please enter email and password'});
+        if (!req.body.email || !req.body.password) {
+            res.json({
+                success: false,
+                message: 'Please enter email and password'
+            });
         } else {
-            let user = new Candidate({
-                name: req.body.name,
+            Candidate.find({
                 email: req.body.email
-            });
-            user.save((err, savedUser) => {
-                if(err){
+            }, (err, doc) => {
+                if (err || doc) {
                     console.log(err);
-                    return res.json({success: false, message: "Email exists", err})
-                }
-                if(savedUser){
-                    let newLogin = new Login({
-                        email: req.body.email,
-                        password: req.body.password,
-                        userId: savedUser._id
+                    return res.json({
+                        success: false,
+                        message: "Email exists"
                     });
-                    newLogin.save((err, saved) => {
-                        if(err){
+                } else {
+                    let user = new Candidate({
+                        name: req.body.name,
+                        email: req.body.email
+                    });
+                    user.save((err, savedUser) => {
+                        if (err) {
                             console.log(err);
-                            return res.json({success: false, message: "Email exists"});
+                            return res.json({
+                                success: false,
+                                message: "Email exists",
+                                err
+                            })
                         }
-                        res.json({success: true, saved});
+                        if (savedUser) {
+                            let newLogin = new Login({
+                                email: req.body.email,
+                                password: req.body.password,
+                                userId: savedUser._id
+                            });
+                            newLogin.save((err, saved) => {
+                                if (err) {
+                                    console.log(err);
+                                    return res.json({
+                                        success: false,
+                                        message: "Email exists"
+                                    });
+                                }
+                                res.json({
+                                    success: true,
+                                    saved
+                                });
+                            });
+                        }
                     });
                 }
             });
+
         }
     },
     login: (req, res) => {
         Login.findOne({
             email: req.body.email
         }, (err, login) => {
-            if(err) throw err;
-            if(!login) res.send({success: false, message: 'Auth failed'})
+            if (err) throw err;
+            if (!login) res.send({
+                success: false,
+                message: 'Auth failed'
+            })
             else {
-                login.comparePassword(req.body.password, (err, isMatch)=>{
-                    if(isMatch && !err) {
+                login.comparePassword(req.body.password, (err, isMatch) => {
+                    if (isMatch && !err) {
                         //login.toObject()
                         let token = jwt.sign({
                             userId: login.userId,
@@ -54,15 +83,22 @@ module.exports = {
                         }, keys.JWT_SECRET, {
                             expiresIn: 100800
                         });
-                        res.json({success: true, token: `${token}`, expiresIn: 100800});
+                        res.json({
+                            success: true,
+                            token: `${token}`,
+                            expiresIn: 100800
+                        });
                     } else {
-                        res.json({success: false, message: 'Auth failed, password'});
+                        res.json({
+                            success: false,
+                            message: 'Auth failed, password'
+                        });
                     }
                 });
             }
         });
     },
     logout: (req, res) => {
-        
+
     }
 }
