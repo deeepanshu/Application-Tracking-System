6dd9e828970c8d45bc4c529178c7ea253a96c87e
Deepanshu Jain <deepanshujain1234@live.com>
2019-03-09 21:50:53 +0530

    Added Env Variables

--- config/prod.js
+++ config/prod.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+    MONGO_URI: process.env.MONGO_URI,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+    AWS_ACL: process.env.AWS_ACL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    SENDGRID: process.env.SENDGRID,
+    HOST_FRONT: process.env.HOST_FRONT,
+    TWO_FACTOR: process.env.TWO_FACTOR
+}
